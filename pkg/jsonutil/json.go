91a29c16f77fcae9226d6a5b184356050abdf8f0
dokshiye <okshieva@gmail.com>
2024-04-08 18:14:26 +0500

    refactor: updating pkg

--- pkg/jsonutil/json.go
+++ pkg/jsonutil/json.go
@@ -2,7 +2,11 @@ package jsonutil
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 type Envelope map[string]any
@@ -22,6 +26,35 @@ func WriteJSON(w http.ResponseWriter, status int, data Envelope, headers http.He
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+
+	return nil
+}
+
+func ReadJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	// http.MaxBytesReader() to limit the size of the request body to 1MB.
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+
+	if err != nil {
+		var maxBytesError *http.MaxBytesError
+
+		switch {
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+		default:
+			return err
+		}
+	}
 
 	return nil
 }
